da3f2e60fc79364b6ab7244b78f2e919bfc30c80
P3PPP <ticktackmob@gmail.com>
2016-06-08 12:21:31 +0900

    initial commit

---
+++ DroidSample/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+namespace DroidSample
+{
+	[Activity(Label = "DroidSample", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			var seekBar = FindViewById<SeekBar>(Resource.Id.seekBar);
+			var textView = FindViewById<TextView>(Resource.Id.textView);
+
+
+			seekBar.ObserveEveryValueChanged(x => x.Progress)
+				.Subscribe(x => textView.Text = x.ToString());
+		}
+	}
+}
+
+
---
+++ ObserveEveryValueChanged/Reactive.Bindings.Extensions/ObserveEveryValueChangedExtensions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Android.Animation;
+
+namespace Reactive.Bindings.Extensions
+{
+	public static class ObserveEveryValueChangedExtensions
+	{
+		/// <summary>
+		/// Publish target property when value is changed. If source is destructed, publish OnCompleted.
+		/// </summary>
+		public static IObservable<TProperty> ObserveEveryValueChanged<TSource, TProperty>(this TSource source, Func<TSource, TProperty> propertySelector, IEqualityComparer<TProperty> comparer = null)
+			where TSource : class
+		{
+			if (source == null) return Observable.Empty<TProperty>();
+			comparer = comparer ?? EqualityComparer<TProperty>.Default;
+
+			var reference = new WeakReference(source);
+			source = null;
+
+			return Observable.Create<TProperty>(observer =>
+			{
+				var currentValue = default(TProperty);
+				var prevValue = default(TProperty);
+
+				var t = reference.Target;
+				if (t != null)
+				{
+					try
+					{
+						currentValue = propertySelector((TSource)t);
+					}
+					catch (Exception ex)
+					{
+						observer.OnError(ex);
+					}
+					finally
+					{
+						t = null;
+					}
+				}
+				else
+				{
+					observer.OnCompleted();
+					return Disposable.Empty;
+				}
+
+				observer.OnNext(currentValue);
+				prevValue = currentValue;
+
+				var valueAnimator = new ValueAnimator();
+				valueAnimator.SetIntValues(0, 100);
+				valueAnimator.RepeatCount = ValueAnimator.Infinite;
+				valueAnimator.Start();
+
+				return Observable.FromEvent<EventHandler<ValueAnimator.AnimatorUpdateEventArgs>, ValueAnimator.AnimatorUpdateEventArgs>(
+					h => (sender, e) => h.Invoke(e),
+					h => valueAnimator.Update += h,
+					h => {
+						valueAnimator.Update -= h;
+						valueAnimator.Dispose();
+					})
+						.Subscribe(_ =>
+						{
+							var target = reference.Target;
+							if (target != null)
+							{
+								try
+								{
+									currentValue = propertySelector((TSource)target);
+								}
+								catch (Exception ex)
+								{
+									observer.OnError(ex);
+								}
+								finally
+								{
+									target = null;
+								}
+							}
+							else
+							{
+								observer.OnCompleted();
+							}
+
+							if (!comparer.Equals(currentValue, prevValue))
+							{
+								observer.OnNext(currentValue);
+								prevValue = currentValue;
+							}
+						});
+			});
+		}
+	}
+}
+
